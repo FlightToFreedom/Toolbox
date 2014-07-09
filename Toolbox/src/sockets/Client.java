0640687a34c630af23a12d51abe1d1be4fa85456
eiorio <eiorio@CSNLEIORIO.office.comscore.com>
2014-07-09 13:50:47 +0200

    Adding a small demo about socket programming, really basic,

--- Toolbox/src/sockets/Client.java
+++ Toolbox/src/sockets/Client.java
@@ -1,5 +1,63 @@
 package sockets;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * This creates a Clinet Socket to connect to a Server @ any location
+ * 
+ * @author eiorio
+ * 
+ */
 public class Client {
 
+	public static void main(String... args) {
+
+		String host = args[0];
+		Integer port = Integer.parseInt(args[1]);
+
+		Socket socket;
+
+		try {
+			System.out
+					.println("... Attempting connection to "
+							+ host
+							+ " on port "
+							+ port);
+			socket = new Socket(host, port);
+			System.out.println("Connected...!");
+			// arrange output stream
+			OutputStream os = socket
+					.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(
+					os);
+
+			dos.writeUTF("Sending stuff from "
+					+ socket.getLocalAddress());
+
+			// arrange the input from the server
+			InputStream is = socket
+					.getInputStream();
+			DataInputStream dis = new DataInputStream(
+					is);
+
+			System.out
+					.println("Receiving from server: "
+							+ dis.readUTF());
+
+			// cose the writer and reader and the socket
+
+			System.out
+					.println("Closing the client, BYE!");
+			dis.close();
+			dos.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Toolbox/src/sockets/Server.java
+++ Toolbox/src/sockets/Server.java
@@ -1,5 +1,92 @@
 package sockets;
 
-public class Server {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 
+/**
+ * Simple represetation of a server-side socket implementation
+ * 
+ * @author eiorio
+ * 
+ */
+public class Server extends Thread {
+
+	private ServerSocket	serverSocket;
+
+	/**
+	 * Construct this
+	 * 
+	 * @param host
+	 * @param port
+	 */
+	public Server(String port) {
+		try {
+			this.serverSocket = new ServerSocket(
+					Integer.parseInt(port));
+			serverSocket.setSoTimeout(50000);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+
+		while (true) {
+			try {
+				System.out
+						.println("Waiting for incoming requests on port.."
+								+ serverSocket
+										.getLocalPort());
+
+				Socket socket = serverSocket
+						.accept();
+				System.out
+						.println("Just connected to.. "
+								+ socket.getRemoteSocketAddress());
+				// Arrange the input from the client
+				InputStream is = socket
+						.getInputStream();
+				DataInputStream dis = new DataInputStream(
+						is);
+				System.out.println(dis.readUTF());
+
+				// Arrange the output to the client
+				OutputStream os = socket
+						.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(
+						os);
+
+				dos.writeUTF("Replying from.. "
+						+ socket.getLocalAddress());
+				dis.close();
+				dos.close();
+				socket.close();
+
+			} catch (SocketTimeoutException stoe) {
+				System.out
+						.println("Socket has timed out");
+				stoe.printStackTrace();
+			} catch (IOException IOex) {
+				IOex.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Start this thread
+	 * 
+	 * @param args
+	 */
+	public static void main(String... args) {
+		String port = args[0];
+		Thread server = new Server(port);
+		server.run();
+	}
 }
