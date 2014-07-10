e67626c7d8a0edbbb2b396f2015e112a8b7bdfb2
eiorio <eiorio@CSNLEIORIO.office.comscore.com>
2014-07-10 11:24:43 +0200

    Adding new small subproject, a multithreaded resource reader

---
+++ Toolbox/src/multithread/resources_reader/lib/Main.java
@@ -0,0 +1,17 @@
+package multithread.resources_reader.lib;
+
+/**
+ * This simple project demonstrates a multithreaded resource reader, it reads
+ * some files with data in the same project, for now, this will not look for
+ * files in the underlying filesystem. Data generated using:
+ * http://www.generatedata.com/
+ * 
+ * @author eiorio
+ * 
+ */
+public class Main {
+
+	public static void main(String... args) {
+
+	}
+}
