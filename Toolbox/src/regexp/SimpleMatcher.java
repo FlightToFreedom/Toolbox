95111396f544fc2b91916697161dd73751fc4ce5
eiorio <eiorio@CSNLEIORIO.office.comscore.com>
2014-09-24 10:09:13 +0200

    Added a regexp match test for a friend

--- Toolbox/src/regexp/SimpleMatcher.java
+++ Toolbox/src/regexp/SimpleMatcher.java
@@ -3,8 +3,8 @@ package regexp;
 public class SimpleMatcher {
 
 	public static void main(String... args) {
-		String regex = "^https?://(shops|support)\\.mobistar\\.be";
-		String vector = "http://shops.mobistar.be/b2c/";
+		String regex = "^https?://e\\-services\\.mobistar\\.be.*";
+		String vector = "http://e-services.mobistar.be/b2c/";
 
 		System.out.println(vector.matches(regex));
 	}
