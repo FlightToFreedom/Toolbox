98a3de1a24e50ebb492a7247443d518370ec409b
eiorio <eiorio@CSNLEIORIO.office.comscore.com>
2015-02-05 12:03:00 +0100

    Added some log lines to recursive method

--- Toolbox/src/recursion/TriangularNumbers.java
+++ Toolbox/src/recursion/TriangularNumbers.java
@@ -3,17 +3,23 @@ package recursion;
 public class TriangularNumbers {
 
 	public static void main(String... args) {
-		System.out
-				.println(getTriangularNumber(4));
+		System.out.println("Final result "
+				+ getTriangularNumber(4));
 	}
 
 	public static int getTriangularNumber(
 			int number) {
+		System.out.println("Entering with: "
+				+ number);
 		if (number == 1) {
 			return 1;
 		} else {
-			return number
+			int tmp = number
 					+ getTriangularNumber(number - 1);
+			System.out
+					.println("Returning...tmp is "
+							+ tmp);
+			return tmp;
 		}
 	}
 }
