b9c5cf1c1c710a2902568d86a54e4b57090e41bf
eiorio <eiorio@CSNLEIORIO.office.comscore.com>
2015-07-08 12:25:33 +0200

    Added a PrintWriter class

--- Toolbox/src/collections/LinkedListTests.java
+++ Toolbox/src/collections/LinkedListTests.java
@@ -1,5 +1,20 @@
 package collections;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
 public class LinkedListTests {
 
+	public static void main(String[] args){
+		List<String> list=new LinkedList<String>();
+		ListIterator listIterator=list.listIterator();
+		Iterator iterator=list.iterator();
+
+		Set<String> set=new HashSet<String>();
+		Iterator setIterator=set.iterator();
+	}
 }
---
+++ Toolbox/src/io/to_file/UsePrintWriter.java
@@ -0,0 +1,5 @@
+package io.to_file;
+
+public class UsePrintWriter {
+
+}
