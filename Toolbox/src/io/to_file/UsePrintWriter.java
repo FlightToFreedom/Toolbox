9970ee4ff51c829b5c89fdc5bbf352adefba8164
eiorio <eiorio@CSNLEIORIO.office.comscore.com>
2015-08-17 10:49:59 +0200

    Added an input and an output file

--- Toolbox/src/io/to_file/UsePrintWriter.java
+++ Toolbox/src/io/to_file/UsePrintWriter.java
@@ -1,6 +1,7 @@
 package io.to_file;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -13,16 +14,27 @@ import java.io.PrintWriter;
 public class UsePrintWriter {
 
 	public static void main(String... args){
-		//lets try to find out the current location
-		String fileName="input.txt";
-		StringBuilder path=new StringBuilder("src/io/to_file/resources/");
-		path.append(fileName);
-		File file=new File(path.toString());
+		//Input and output filenames
+		String inputFileName="input.txt";
+		String outputFileName="output.txt";
+
+		//build a path object pointing to the resources dir
+		final String path=new String("src/io/to_file/resources/");
+		String inputFilePath=path.concat(inputFileName);
+		String outputFilePath=path.concat(outputFileName);
+
+		File inputFile=new File(inputFilePath);
+		File outputFile=new File(outputFilePath);
 
 		PrintWriter writer;
+		FileReader reader;
 		try{
-			writer=new PrintWriter(file);
+			//init the writer
+			writer=new PrintWriter(outputFile);
+			//init the reader
+			reader=new FileReader(inputFile);
 
+			//read the input file
 		}catch(IOException e){
 			e.printStackTrace();
 		}
