ec623777ca45b8f3f7331a6c5ac598ad2e8bd58a
eiorio <eiorio@CSNLEIORIO.office.comscore.com>
2015-08-17 11:54:34 +0200

    Added an example of usage of PrintWriter and BufferedReader

--- Toolbox/src/io/to_file/UsePrintWriter.java
+++ Toolbox/src/io/to_file/UsePrintWriter.java
@@ -1,5 +1,6 @@
 package io.to_file;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -7,7 +8,7 @@ import java.io.PrintWriter;
 
 
 /**
- * Simple class that explains the usage of the PrintWriter class
+ * Simple class that explains the usage of the PrintWriter class, it uses a BufferedReader to read an input file and prints it into another "output" file using a PrintWriter
  * @author eiorio
  *
  */
@@ -26,17 +27,26 @@ public class UsePrintWriter {
 		File inputFile=new File(inputFilePath);
 		File outputFile=new File(outputFilePath);
 
-		PrintWriter writer;
-		FileReader reader;
+		PrintWriter writer=null;
+		BufferedReader reader=null;
 		try{
 			//init the writer
 			writer=new PrintWriter(outputFile);
 			//init the reader
-			reader=new FileReader(inputFile);
+			reader=new BufferedReader(new FileReader(inputFile));
 
 			//read the input file
+			String line=null;
+			if(outputFile.canWrite() && inputFile.canRead())
+				while((line=reader.readLine())!=null)
+					writer.println(line);
+
+			reader.close();
 		}catch(IOException e){
 			e.printStackTrace();
+		}finally{
+			//release any resource associated with this
+			writer.close();
 		}
 
 	}
