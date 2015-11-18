ee731d77d98c33e56d7a35864133532263144c79
Enrico <enrico@192.168.100.11>
2015-11-18 07:17:26 +0100

    Added an ArraySwapper

---
+++ Toolbox/src/algorithms/ArraySwapper.java
@@ -0,0 +1,55 @@
+package algorithms;
+
+public final class ArraySwapper<T> {
+
+
+
+	//Swaps an array
+
+	public static <T> T[] swap(T[] array){
+
+		int size=(array.length-1);
+
+		//determine the half 
+
+		int half=size/2;
+
+
+
+		T tmp;
+
+		int i=size;
+
+		while(i>half){
+
+			tmp=array[i];
+
+			//swap
+
+			array[i]=array[size-i];
+
+			array[size-i]=tmp;
+
+			i--;
+
+		}
+
+		return array;
+
+	}
+
+	//1 | 2 | 3 | 4 | 5 | 6
+
+	public static void main(String[] args){
+
+		String[] array=swap(new String[]{"a","f","p","u","y","z"});
+
+		for(String element:array){
+
+			System.out.println(element);
+
+		}
+
+	}
+
+}
